a483b2243908576ebe6b6a43af445d35cb412d36
Patryk Malek <malekpatryk@gmail.com>
2014-06-08 04:16:43 +0200

    Table with points made scrollable and changed layout to 2 tabs.

--- src/pl/wroc/pwr/patryk/MainActivity.java
+++ src/pl/wroc/pwr/patryk/MainActivity.java
@@ -145,8 +145,8 @@ public class MainActivity extends Activity implements ActionBar.TabListener {
 
 		@Override
 		public int getCount() {
-			// Show 3 total pages.
-			return 3;
+			// Show 2 total pages.
+			return 2;
 		}
 
 		@Override
